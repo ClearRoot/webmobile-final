fbc80873b2c2762cc99b7827f4597705a380efb8
kimseonili <rain6781@naver.com>
2019-07-23 13:52:45 +0900

    S1P3113003-16 깃랩 토큰 갱신 및 백오피스 페이지 BackOfficePage.vue 생성

---
+++ src/views/BackOfficePage.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <ImgBanner>
+      <div style="line-height:1.2em;font-size:1.2em;" slot="text">
+        Portfolio
+      </div>
+    </ImgBanner>
+    <v-container>
+      <!-- Portfolio -->
+      <!-- here -->
+      <!-- Portfolio -->
+      <v-layout>
+        <v-flex xs12>
+          <PortfolioList :limits="6" :load-more="true"></PortfolioList>
+        </v-flex>
+      </v-layout>
+      <v-layout>
+        <v-flex xs12></v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import ImgBanner from "../components/ImgBanner";
+
+export default {
+  name: "BackOfficePage",
+  components: {
+    ImgBanner
+  },
+  data() {
+    return {
+      ddlSource: "ko",
+      ddlTarget: "en"
+    };
+  },
+  methods: {
+    translate() {
+      const axios = require("axios");
+      var translateUrl =
+        "https://www.googleapis.com/language/translate/v2?key=AIzaSyChUf-_S1c5gnxJdSZE8u5hBjTyRlBSgm8";
+      translateUrl += "&source=" + this.ddlSource;
+      translateUrl += "&target=" + this.ddlTarget;
+      translateUrl += "&q=" + encodeURI(this.title);
+      translateUrl += "&q=" + encodeURI(this.body);
+      axios({
+        methods: "GET",
+        url: translateUrl
+      }).then(res => {
+          this.title = res.data.data.translations[0].translatedText;
+          this.body = res.data.data.translations[1].translatedText;
+          if (this.ddlSource == "en") {
+            this.ddlSource = "ko";
+            this.ddlTarget = "en";
+          } else {
+            this.ddlSource = "en";
+            this.ddlTarget = "ko";
+          }
+        }).catch(e => {
+          console.error(e)
+        });
+    }
+  }
+};
+</script>
