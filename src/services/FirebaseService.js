5e82165f8afd203088dc915d37c1db483a96e7e2
Jo Hogeun <jhk7767@naver.com>
2019-07-29 13:22:13 +0900

    S1P3113003-13 저장되는 유저권한정보에서 시간정보를 하나로 통합

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -184,7 +184,6 @@ export default {
       .set({
         userEmail: firebase.auth().currentUser.email,
         userAuth: "visitant",
-        created_at: firebase.firestore.FieldValue.serverTimestamp(),
         updated_at: firebase.firestore.FieldValue.serverTimestamp()
       });
   },
