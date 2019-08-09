822cbc1870d59c030ccd4435286f684ab70b52c5
Jo Hogeun <jhk7767@naver.com>
2019-08-09 15:24:10 +0900

    Bugfix 정상적인 Hosting을 위해 index.html 코드 수정

--- src/components/AnalyticsTab.vue
+++ src/components/AnalyticsTab.vue
@@ -35,29 +35,14 @@
   </div>
 </template>
 <script>
-(function(w, d, s, g, js, fs) {
-  g = w.gapi || (w.gapi = {});
-  g.analytics = {
-    q: [],
-    ready: function(f) {
-      this.q.push(f);
-    }
-  };
-  js = d.createElement(s);
-  fs = d.getElementsByTagName(s)[0];
-  js.src = "https://apis.google.com/js/platform.js";
-  fs.parentNode.insertBefore(js, fs);
-  js.onload = function() {
-    g.load("analytics");
-  };
-})(window, document, "script");
 import ApiService from "@/services/ApiService";
+
 export default {
   name: "AnalyticsTab",
-  data(){
-    return{
+  data() {
+    return {
       date: null
-    }
+    };
   },
   mounted() {
     this.date = new Date();
